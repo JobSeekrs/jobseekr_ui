d9d9564c419c65a8ac4d320d7eec454af954ffa3
Niv Salamon <mnsalamon@gmail.com>
2017-12-04 19:57:24 -0800

    Navbar toggling. Routes WIP

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor(props) {
+         super(props);
+         this.toggleClass = this.toggleClass.bind(this);
+         this.state = {
+             active: false,
+         };
+     }
+
+     toggleClass() {
+       console.log('toggleClass function in here');
+      var currentState = this.state.active;
+      this.setState({ active: !currentState });
+     };
+
+
   render() {
     return (
       <div>
-          <h1>HELLOOO</h1>
+        <div id="wrapper" className={ this.state.active ? 'toggled': 'notToggled' }>
+        <Navigation/>
+          <div id="page-content-wrapper">
+                <div className="container-fluid">
+                      <button className="btn btn-secondary" id="menu-toggle"
+                         onClick={this.toggleClass}>
+                        Toggle Menu
+                      </button>
+                      <h3>Your Name</h3>
+
+                    <p>This template hathe menu wil
+                        small screens,
+                        the page content will be pushed off canvas.
+                      </p>
+                </div>
+          </div>
       </div>
     );
   }
