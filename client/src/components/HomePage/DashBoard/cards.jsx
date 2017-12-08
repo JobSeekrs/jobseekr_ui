c6a89c757d8dbc5c7d2c9e6c064c3df1d90aa74c
Niv Salamon <mnsalamon@gmail.com>
2017-12-07 19:05:47 -0800

    links dashboard job links opening ina  new tab correctly

--- client/src/components/HomePage/DashBoard/cards.jsx
+++ client/src/components/HomePage/DashBoard/cards.jsx
@@ -9,7 +9,7 @@ const Cards = (props) => (
           <div className="card-inner">
             <h6 className="job_company">{job.companyId}</h6>
             <h6 className="job_title">{job.name}</h6>
-            <a className="original_job_url" href={job.link}>UPDATE Name of will Go here</a>
+            <a className="original_job_url" href={'http://'+job.link} target="_blank">UPDATE Name of will Go here</a>
             <p className="personal_rating">Rating: {job.priority}</p>
             <div className="last_applied">
               <h6 className="last_applied_text">6 months ago UPDATE</h6>
