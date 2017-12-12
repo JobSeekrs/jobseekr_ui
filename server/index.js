35e096698a2246fda254f9e26b6606c5f1a87298
Brent Eickhoff <dev@eickhoffs.net>
2017-12-12 15:30:11 -0800

    mplement signup (mvp), add CORS, try dotenv failed (WIP)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../client/public/'), bodyparser()));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/public/index.html'));
