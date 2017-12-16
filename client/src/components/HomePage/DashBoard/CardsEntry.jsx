f69393fd72e9fd277deda7ba15fbbcf3ae290546
Niv Salamon <mnsalamon@gmail.com>
2017-12-15 20:04:38 -0800

    redux for jobdetails implemented with endpoint

--- client/src/components/HomePage/DashBoard/CardsEntry.jsx
+++ client/src/components/HomePage/DashBoard/CardsEntry.jsx
@@ -29,7 +29,7 @@ class CardsEntry extends React.Component {
           <p className="job_status">Status: {this.props.job.status}</p>
           <p className="job_deadline">Deadline: {<TimeAgo date={this.props.job.deadline} /> } </p>
           <div className="last_applied">
-          <h6 className="last_applied_text">{<TimeAgo date={this.props.job.created_date} /> }</h6>
+          <h6 className="last_applied_text">Created: {<TimeAgo date={this.props.job.created_date} /> }</h6>
           </div>
         </div>
       </div>
--- client/src/reducers/jobDetailsAdditionalReducer.jsx
+++ client/src/reducers/jobDetailsAdditionalReducer.jsx
@@ -1,6 +1,5 @@
 const jobDetailsAdditional = (state = null, action) => {
     if (action.type === 'JOB_DETAILS_ADDITIONAL') {
-        console.log('action.payload: ', action.payload);
         return action.payload;
     }
     return state;
